43f6a20fd8b996f3915f38204fcb5e45292cba6d
Dipto <diptomondal007@gmail.com>
2020-02-03 15:31:56 +0600

    Update dictionary.go

--- dictionary.go
+++ dictionary.go
@@ -7,23 +7,25 @@ type Dictionary struct {
 }
 
 func New() *Dictionary{
-	return new(Dictionary)
+	newDictionary := new(Dictionary)
+	newDictionary.Entries = make(map[string]string)
+	return newDictionary
 }
 
 func(d Dictionary) Add(word string, definition string){
 	if _, ok := d.Entries[word];ok{
 		d.Entries[word] = definition
-		fmt.Printf("%v is updated with the defintion %v !",word,definition)
+		fmt.Printf("%v is updated with the defintion %v !\n",word,definition)
 	}else {
 		d.Entries[word] = definition
-		fmt.Printf("%v is inserted",word)
+		fmt.Printf("%v is inserted\n",word)
 	}
 }
 
 func (d Dictionary) Delete(word string)  {
 	if _, ok := d.Entries[word];ok{
 		delete(d.Entries, word)
-		fmt.Printf("%v is deleted",word)
+		fmt.Printf("%v is deleted\n",word)
 	}else {
 		fmt.Println("Sorry! Not Found. Deletion not successful.")
 	}
@@ -31,9 +33,9 @@ func (d Dictionary) Delete(word string)  {
 
 func (d Dictionary) Search(word string){
 	if definition, ok := d.Entries[word];ok{
-		fmt.Println(definition)
+		fmt.Printf("The defintion for the word %v is %v\n",word,definition)
 	}else {
-		fmt.Println("Sorry Not Found")
+		fmt.Printf("Sorry %v not found",word)
 	}
 }
 
