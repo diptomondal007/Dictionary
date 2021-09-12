2d4ab6b8931f56c9eff56b21f46f6b1a47fc8bf0
Dipto Mondal <36878760+diptomondal007@users.noreply.github.com>
2021-09-12 16:57:54 +0600

    Felt embarrassed seeing old bad code

--- dictionary.go
+++ dictionary.go
@@ -12,35 +12,31 @@ func(d Dictionary) Add(word string, definition string) (string , error){
 	if _, ok := d[word];!ok{
 		d[word] = definition
 		return word , nil
-	}else {
-		return word ,fmt.Errorf("%v already exists",word)
 	}
+	return word ,fmt.Errorf("%v already exists",word)
 }
 
 func (d Dictionary) Update(word string, definition string)(string, error){
 	if _, ok := d[word];ok{
 		d[word] = definition
 		return fmt.Sprintf("%v is updated",word),nil
-	}else{
-		return word,fmt.Errorf("sorry %v not found can not be updated",word)
 	}
+	return word,fmt.Errorf("sorry %v not found can not be updated",word)
 }
 
 func (d Dictionary) Delete(word string) (string ,error) {
 	if _, ok := d[word];ok{
 		delete(d, word)
 		return fmt.Sprintf("%v deleted",word),nil
-	}else {
-		return word,fmt.Errorf("sorry %v not found",word)
 	}
+	return word,fmt.Errorf("sorry %v not found",word)
 }
 
 func (d Dictionary) Search(word string) (string , error){
 	if definition, ok := d[word];ok{
 		return definition,nil
-	}else {
-		return word,fmt.Errorf("sorry %v not found",word)
 	}
+	return word,fmt.Errorf("sorry %v not found",word)
 }
 
 
